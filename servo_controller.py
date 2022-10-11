bc148955a2487b2a9ba3f5b17b91d05b6a0ba2de
LucaTaglialatela <45466948+LucaTaglialatela@users.noreply.github.com>
2022-10-11 19:45:11 +0800

    Fixed issue of some commands being ignored

--- servo_controller.py
+++ servo_controller.py
@@ -1,4 +1,3 @@
-from multiprocessing.connection import wait
 import serial
 from uservo import UartServoManager
 import argparse
@@ -47,52 +46,56 @@ incident_angles = {
 	90 : 45
 }
 
+# Get current servo angle rounded to the nearest multiple of 15 degrees
 def getCurrentAngle(uservo):
 	return 15 * round(uservo.query_servo_angle(SERVO_1_ID) / 15)
 
+# Rotate servo 90 degrees counterclockwise
 def resetOffset(uservo):
-	uservo.set_wheel_time(SERVO_1_ID, interval=2000, is_cw=False, mean_dps=287.5)
+	uservo.set_wheel_time(SERVO_1_ID, interval=2000, is_cw=False, mean_dps=287.5, is_wait=True)
 
+# Rotate servo 90 degrees clockwise
 def setOffset(uservo):
-	uservo.set_wheel_time(SERVO_1_ID, interval=2000, is_cw=True, mean_dps=287.5)
+	uservo.set_wheel_time(SERVO_1_ID, interval=2000, is_cw=True, mean_dps=287.5, is_wait=True)
 
-def setAngle(uservo, angle, offset):
-	global DANGERZONE
-	
-	print('Angle before rotation: ', getCurrentAngle(uservo))
-	
-	# Reset the offset if previous angle >= 270 (270 is mapped to 150 by the servo)
-	if DANGERZONE:
-		print('\nResetting offset, please wait...')
-		resetOffset(uservo)
-		DANGERZONE = False
-
-	# Move the servo to the relative angle specified by the second parameter
+# Set the rotational angle of the servo
+def setAngle(uservo, angle):
 	if getCurrentAngle(uservo) != angle:
 		uservo.set_servo_angle(SERVO_1_ID, angle, interval=0)
 		uservo.wait()
-	
-	# If offset is set, move the servo an additional 90 degrees in the specified direction
-	if offset:
-		# uservo.set_wheel_time(SERVO_1_ID, interval=2000, is_cw=(angle >= 0), mean_dps=287.5)
-		uservo.set_wheel_time(SERVO_1_ID, interval=2000, is_cw=True, mean_dps=287.5)
-		DANGERZONE = True
 
-	print('Angle after rotation: ', getCurrentAngle(uservo), DANGERZONE)
-
+# Set the incidental angle of the servo
 def setIncidentAngle(uservo, angle):
 	uservo.set_servo_angle(SERVO_2_ID, angle, interval=0)
 	uservo.wait()
 
+# Pause the program until `Enter` is pressed
 def waitForInput(message):
 	cmd = input(message)
 	while cmd != '':
 		cmd = input(message)
 
+# Notify participant
+def notify():
+	waitForInput('Press enter to notify participant: ')
+	winsound.Beep(1000, 600)  # Beep at 1000 Hz for 600 ms
+
+def manualInput():
+	# Optionally make manual servo angle adjustments in case of inacurracies
+	cmd = input('Manually adjust rotational angle? (y/n): ')
+	while cmd != 'n':
+		if cmd == 'y':
+			angle = int(input('Enter rotational angle: '))
+			setAngle(uservo, *angles[angle])
+		if cmd == 'reset':
+			resetOffset(uservo)
+		cmd = input('Manually adjust rotational angle? (y/n): ')
+
 if __name__=='__main__':
 	### PROGRAM INIT ###
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-s', '--set_angles', type=int, nargs=2, default=[-1, -1])
+	parser.add_argument('-r', '--reset_offset', action='store_true')
+	parser.add_argument('-i', '--incident_angle', type=int, nargs=1, default=[-1])
 	parser.add_argument('-f', '--file', type=str, nargs=1, default=[''])
 	parser.add_argument('-skip', '--skip', type=int, nargs=1, default=[-1])
 	args = parser.parse_args()
@@ -103,17 +106,17 @@ if __name__=='__main__':
 	uservo = UartServoManager(uart, is_debug=True)
 
 	# If set flag is set, return servo to custom position specified by user
-	if not args.set_angles[0] < 0 and not args.set_angles[1] < 0:
-		if abs(incident_angles[args.set_angles[0]] - uservo.query_servo_angle(SERVO_2_ID)) > 2.0:
-			setIncidentAngle(uservo, incident_angles[args.set_angles[0]])
-		setAngle(uservo, *angles[args.set_angles[1]])
+	if not args.incident_angle[0] < 0:
+		if abs(incident_angles[args.incident_angle[0]] - uservo.query_servo_angle(SERVO_2_ID)) > 2.0:
+			setIncidentAngle(uservo, incident_angles[args.incident_angle[0]])
 		exit()
 
-	# resetOffset(uservo)
-	# exit()
+	if args.reset_offset:
+		resetOffset(uservo)
+		exit()
 
 	# Set the servo to neutral state
-	setAngle(uservo, *angles[180])
+	setAngle(uservo, angles[180][0])
 
 	### PROGRAM START ###
 	waitForInput('Press enter to start: ')
@@ -121,40 +124,49 @@ if __name__=='__main__':
 	filename = args.file[0] + '_order.txt'
 	f = open(os.path.join(sys.path[0], 'orders', filename), encoding = "utf8")
 	
+	# Determine starting line in file
 	if args.skip[0] > 0:
 		for i in range(args.skip[0] + 1):
 			next(f)
 	else:
-		next(f) # Skip first line of file
+		next(f)
 
+	# Execute samples
 	sample_count = 0
 	for line in f:
 		sample = line.strip().split(',')
-		angle = int(sample[2])
+		target_angle = int(sample[2])
+		target_angle_mapped = angles[int(sample[2])][0]
+		offset = angles[int(sample[2])][1]
 
-		print('\n' + str(sample_count % 40 + 1) + '# sample ' + str(sample))
+		print('\n' + str(sample_count % 60 + 1) + '# sample ' + str(sample))
+	
+		# Reset the offset if it was set previously
+		while DANGERZONE:
+			print('Resetting offset, please wait...')
+			resetOffset(uservo)
+			DANGERZONE = False
 
 		# Rotate the servo to the right angle
-		print('Setting rotational angle to ' + str(angle) + ' degrees')
-		setAngle(uservo, *angles[angle])
+		# print('Setting rotational angle to ' + str(target_angle) + ' degrees')
+		while(getCurrentAngle(uservo) != target_angle_mapped):
+			setAngle(uservo, target_angle_mapped)
 
-		# Optionally make manual servo angle adjustments in case of inacurracies
-		cmd = input('Manually adjust rotational angle? (y/n): ')
-		while cmd != 'n':
-			if cmd == 'y':
-				angle = int(input('Enter rotational angle: '))
-				setAngle(uservo, *angles[angle])
-			if cmd == 'reset':
-				resetOffset(uservo)
-			cmd = input('Manually adjust rotational angle? (y/n): ')
+		# If offset is set, move the servo an additional 90 degrees in the specified direction
+		if offset:
+			print('Setting offset, please wait...')
+			# uservo.set_wheel_time(SERVO_1_ID, interval=2000, is_cw=(angle >= 0), mean_dps=287.5)
+			uservo.set_wheel_time(SERVO_1_ID, interval=2000, is_cw=True, mean_dps=287.5)
+			DANGERZONE = True
 
-		# Notify participant
-		waitForInput('Press enter to notify participant: ')
-		winsound.Beep(1000, 600)  # Beep at 1000 Hz for 600 ms
+		# manualInput()
+
+		print('Current angle: ', target_angle)
+		# notify()
 
 		# Continue to next sample
 		waitForInput('Press enter for the next sample: ')
-
+	
 		sample_count += 1
 
 	f.close()
